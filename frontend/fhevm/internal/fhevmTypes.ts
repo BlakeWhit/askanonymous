0ed85587d2f795a5db70609a79493db91afcee1e
SawyerThomp <hathuytu248@gmail.com>
2025-09-21 11:12:31 +0800

    internal types

---
+++ frontend/fhevm/internal/fhevmTypes.ts
@@ -0,0 +1,10 @@
+export type { FhevmInstance } from "../fhevmTypes";
+export type {
+  FhevmLoadSDKType,
+  FhevmInitSDKOptions,
+  FhevmInitSDKType,
+  FhevmRelayerSDKType,
+  FhevmWindowType,
+} from "../fhevmTypes";
+
+
